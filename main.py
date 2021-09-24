af86da2854dba2066bb39a00cb78c7ad66f7689c
ymotkpo3 <tomasdelgadoh@gmail.com>
2021-09-24 20:48:56 -0300

    condicional agregado
    si el valor del calculo que esta dentro de la raíz da negativo, el resultado del calculo no se hará

--- main.py
+++ main.py
@@ -6,8 +6,11 @@ c = float(input('introduce el valor de c: '))
 
 x = (b**2)-(4*a*c)
 
-calculoPositivo = (-b + math.sqrt(x)) / (2*a)
-calculoNegativo = (-b - math.sqrt(x)) / (2*a)
+if x < 0:
+    print("no se puede resolver este calculo porque no se pueden hacer raices negativas")
+else:
+    calculoPositivo = (-b + math.sqrt(x)) / (2*a)
+    calculoNegativo = (-b - math.sqrt(x)) / (2*a)
 
-print("el resultado del calculo con suma es: " + str(calculoPositivo))
-print("el resultado del calculo con resta es: " + str(calculoNegativo))
+    print("el resultado del calculo con suma es: " + str(calculoPositivo))
+    print("el resultado del calculo con resta es: " + str(calculoNegativo))
